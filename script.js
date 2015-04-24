a69eb88de17d9e85f507eba9e0974c9247d3861a
Terry Gunderson <tgunderson25@gmail.com>
2015-04-24 18:45:09 -0500

    Added button divs to html and array of people in javascript

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 /**
  * Created by Prime on 4/24/15.
  */
+var people = ["Erik", "Aaron", "Alicia", "Casie", "Clare", "Cody", "Jeanne", "Kaitlin", "Kelly", "Luke", "Mary", "Michael", "Michelle", "Rom", "Steve", "Terry", "Tracy", "Vince", "Brian", "Chelsea"];
+
+var team2 = [];
+var team3 = [];
+var team4 = [];
+var team5 = [];
+var team6 = [];
+var team7 = [];
+var team8 = [];
+var team9 = [];
+var team10 = [];
