a1419883dc9eab9a78f7e2bafc97fc39bf339aa4
Terry Gunderson <tgunderson25@gmail.com>
2015-04-24 21:44:28 -0500

    added array shuffling funvction

--- script.js
+++ script.js
@@ -12,3 +12,13 @@ var team7 = [];
 var team8 = [];
 var team9 = [];
 var team10 = [];
+
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+}
